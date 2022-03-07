c5d101eec18aeee291ca8f97695d9ad5f41cc18b
Andrew Orals <nd-0r@andreworals.com>
2022-03-07 16:06:57 -0600

    implement basic serial input

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,50 +1,11 @@
 #include "MainComponent.h"
 
-class WavetableOscillator {
-public:
-  WavetableOscillator (const juce::AudioSampleBuffer& wavetableToUse)
-      : wavetable (wavetableToUse)
-  {
-      jassert (wavetable.getNumChannels() == 1);
-  }
-  void setFrequency (float frequency, float sampleRate)
-  {
-      auto tableSizeOverSampleRate = (float) wavetable.getNumSamples() / sampleRate;
-      tableDelta = frequency * tableSizeOverSampleRate;
-  }
-  float getNextSample() noexcept
-  {
-      auto tableSize = (unsigned int) wavetable.getNumSamples();
-
-      auto index0 = (unsigned int) currentIndex;
-      auto index1 = index0 == (tableSize - 1) ? (unsigned int) 0 : index0 + 1;
-
-      auto frac = currentIndex - (float) index0;
-
-      auto* table = wavetable.getReadPointer (0);
-      auto value0 = table[index0];
-      auto value1 = table[index1];
-
-      auto currentSample = value0 + frac * (value1 - value0);
-
-      if ((currentIndex += tableDelta) > (float) tableSize)
-          currentIndex -= (float) tableSize;
-
-      return currentSample;
-  }
-private:
-    const juce::AudioSampleBuffer& wavetable;
-    float currentIndex = 0.0f, tableDelta = 0.0f;
-};
-
 //==============================================================================
 MainComponent::MainComponent()
 {
     // Make sure you set the size of the component after
     // you add any child components.
     setSize (800, 600);
-
-  createSawWavetable();
 
     // Some platforms require permissions to open input channels so request that here
     if (juce::RuntimePermissions::isRequired (juce::RuntimePermissions::recordAudio)
@@ -58,14 +19,47 @@ MainComponent::MainComponent()
         // Specify the number of input and output channels that we want to open
         setAudioChannels (2, 2);
     }
+  
+  // GUI stuffs
   addAndMakeVisible(&levelSlider);
   levelSlider.setRange(0.0f, 1.0f);
   addAndMakeVisible(&freqSlider);
   freqSlider.setRange(20.0f, 12000.0f);
   freqLabel.attachToComponent(&freqSlider, true);
-//  addAndMakeVisible(&qSlider);
-//  qSlider.setRange(0.01f, 100.0f);
-//  qLabel.attachToComponent(&qSlider, true);
+  addAndMakeVisible(&qSlider);
+  qSlider.setRange(0.01f, 100.0f);
+  qLabel.attachToComponent(&qSlider, true);
+  
+  DebugFunction df = [](juce::String a, juce::String b) {
+    juce::Logger* logger = juce::Logger::getCurrentLogger();
+    logger->outputDebugString("---juce_serialport---");
+    logger->outputDebugString("a: " + a);
+    logger->outputDebugString("b: " + b);
+  };
+
+  //open the specified port on the system
+  sp = std::unique_ptr<SerialPort>(new SerialPort(
+    "/dev/ttys003",
+    SerialPortConfig(9600,
+                     8,
+                     SerialPortConfig::SERIALPORT_PARITY_NONE,
+                     SerialPortConfig::STOPBITS_1,
+                     SerialPortConfig::FLOWCONTROL_NONE),
+    df
+  ));
+  if(sp->exists()) {
+    //create stream for reading
+    instream = std::unique_ptr<SerialPortInputStream>(
+      new SerialPortInputStream(sp.get())
+    );
+
+    //ask to be notified whenever any character is received
+    instream->addChangeListener(this);
+    instream->setNotify(SerialPortInputStream::NOTIFY_ON_CHAR);
+    juce::Logger::getCurrentLogger()->writeToLog("opened serial port");
+  } else {
+    juce::Logger::getCurrentLogger()->writeToLog("NO SERIAL PORT FOUND!!!");
+  }
 }
 
 MainComponent::~MainComponent()
@@ -86,13 +80,6 @@ void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRat
     // For more details, see the help for AudioProcessor::prepareToPlay()
   sample_rate = sampleRate;
 
-  osc = new WavetableOscillator(sawTable);
-
-  auto midiNote = juce::Random::getSystemRandom().nextDouble() * 36.0 + 48.0;
-  auto frequency = 440.0 * pow (2.0, (midiNote - 69.0) / 12.0);
-
-  osc->setFrequency ((float) frequency, (float) sampleRate);
-  
   juce::String message;
   message << "Preparing to play audio with ";
   message << samplesPerBlockExpected << " samples per block\n";
@@ -110,20 +97,18 @@ void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& buffe
     // (to prevent the output of random noise)
   auto level = (float) levelSlider.getValue();
   auto freq = (float) freqSlider.getValue();
-//  auto q = juce::jmax((float) qSlider.getValue(), 0.01f);
+  auto q = juce::jmax((float) qSlider.getValue(), 0.01f);
   
-//  bp_filter.setCoefficients(
-//      juce::IIRCoefficients::makeBandPass(sample_rate, freq, q));
-  osc->setFrequency(freq, sample_rate);
+  bp_filter.setCoefficients(
+      juce::IIRCoefficients::makeBandPass(sample_rate, freq, q));
 
   bufferToFill.clearActiveBufferRegion();
   for (auto channel = 0; channel < bufferToFill.buffer->getNumChannels(); ++channel) {
     auto* buffer = bufferToFill.buffer->getWritePointer(channel);
     for (auto sample = 0; sample < bufferToFill.numSamples; ++sample) {
-//      buffer[sample] = level * (random.nextFloat() * 0.25f - 0.125f); // noise
-      buffer[sample] = level * osc->getNextSample();
+      buffer[sample] = level * (random.nextFloat() * 0.25f - 0.125f); // noise
     }
-//    bp_filter.processSamples(buffer, bufferToFill.numSamples); // filter
+    bp_filter.processSamples(buffer, bufferToFill.numSamples); // filter
   }
     
 }
@@ -152,28 +137,21 @@ void MainComponent::resized()
   levelSlider.setSize(200, 50);
   freqSlider.setTopLeftPosition(10, 60);
   freqSlider.setSize(200, 50);
-//  qSlider.setTopLeftPosition(10, 110);
-//  qSlider.setSize(200, 50);
+  qSlider.setTopLeftPosition(10, 110);
+  qSlider.setSize(200, 50);
     // This is called when the MainContentComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
 }
 
-void MainComponent::createSawWavetable() {
-  sawTable.setSize(1, (int) tableSize);
-  auto* sample_arr = sawTable.getWritePointer(0);
-  
-  // Sine
-  auto angleDelta = juce::MathConstants<double>::twoPi / ((double) tableSize - 1);
-  /*
-  // Saw
-  auto angleDelta = 1.0 / (double) (tableSize - 1);
-  */
-  auto currAngle = 0.0;
-  
-  for (size_t i = 0; i < tableSize; ++i) {
-    auto sample = currAngle;
-    sample_arr[i] = (float) sample;
-    currAngle += angleDelta;
+void MainComponent::changeListenerCallback(juce::ChangeBroadcaster* source) {
+  juce::Logger::getCurrentLogger()->outputDebugString("HERE");
+  if (source == instream.get()) {
+    juce::Logger::getCurrentLogger()->outputDebugString("HERE");
+    char* buf = new char[16];
+    instream->read(buf, 16);
+    long new_val = strtol(buf, nullptr, 10);
+    levelSlider.setValue(
+        juce::jmin((double) new_val / 100.0, levelSlider.getMaximum()));
   }
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -10,7 +10,7 @@
 */
 class WavetableOscillator;
 
-class MainComponent  : public juce::AudioAppComponent
+class MainComponent  : public juce::AudioAppComponent, public juce::ChangeListener
 {
 public:
     //==============================================================================
@@ -21,30 +21,30 @@ public:
     void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
     void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override;
     void releaseResources() override;
+    void changeListenerCallback(juce::ChangeBroadcaster* source) override;
 
     //==============================================================================
     void paint (juce::Graphics& g) override;
     void resized() override;
 
 private:
-  void createSawWavetable();
     //==============================================================================
-//    juce::Random random;
-//    juce::IIRFilter bp_filter;
-//    juce::IIRCoefficients filter_params;
-  const unsigned int tableSize = 1 << 7;
-  juce::AudioSampleBuffer sawTable;
-  WavetableOscillator* osc;
+    juce::Random random;
+    juce::IIRFilter bp_filter;
+    juce::IIRCoefficients filter_params;
 
     juce::Slider levelSlider{juce::Slider::SliderStyle::LinearHorizontal,
                              juce::Slider::TextEntryBoxPosition::TextBoxBelow};
     juce::Slider freqSlider{juce::Slider::SliderStyle::LinearHorizontal,
                             juce::Slider::TextEntryBoxPosition::TextBoxBelow};
     juce::Label freqLabel;
-//    juce::Slider qSlider{juce::Slider::SliderStyle::LinearHorizontal,
-//                            juce::Slider::TextEntryBoxPosition::TextBoxBelow};
-//    juce::Label qLabel;
+    juce::Slider qSlider{juce::Slider::SliderStyle::LinearHorizontal,
+                            juce::Slider::TextEntryBoxPosition::TextBoxBelow};
+    juce::Label qLabel;
     double sample_rate = 48000.0;
+  
+    std::unique_ptr<SerialPort> sp;
+    std::unique_ptr<SerialPortInputStream> instream;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
