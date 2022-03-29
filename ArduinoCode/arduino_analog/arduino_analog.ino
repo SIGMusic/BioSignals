fb51334212895bf7562850282bf92263305c1933
Soumilsg <41214985+Soumilsg@users.noreply.github.com>
2022-03-29 16:34:55 -0500

    DEV: Added Code for Second Temperature Sensor

--- ArduinoCode/arduino_analog/arduino_analog.ino
+++ ArduinoCode/arduino_analog/arduino_analog.ino
@@ -18,12 +18,29 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A1);
+  //Temperature Sensor 1:
+  int sensorValue = analogRead(A1);   // read the input on analog pin 0:
+  
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
   float voltage = sensorValue * (5 / 1023.0);
+  
   float temp = ((voltage*1000)-500) / 10; // Celsius
+  
   // print out the value you read:
+  Serial.println("Dummy Signal TS1: ");
   Serial.println(temp);
-  Serial.println("Dummy Signal");
+
+
+  //Temperature Sensor 2:
+  int sensorValue = analogRead(A2);   // read the input on analog pin 0:
+
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float voltage = sensorValue * (5 / 1023.0);
+  
+  float temp = ((voltage*1000)-500) / 10; // Celsius
+  
+  // print out the value you read:
+  Serial.println("Dummy Signal TS2: ");
+  Serial.println(temp);
+
 }
