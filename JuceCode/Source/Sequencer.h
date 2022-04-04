5e5a879a0392907e6048e54335aab1bd4d164e41
Andrew Orals <nd-0r@andreworals.com>
2022-04-04 16:10:31 -0500

    Create sequencer interface

---
+++ JuceCode/Source/Sequencer.h
@@ -0,0 +1,38 @@
+/*
+  ==============================================================================
+
+    Arpeggiator.h
+    Created: 28 Mar 2022 4:57:13pm
+    Author:  Andrew Orals
+
+  ==============================================================================
+*/
+#pragma once
+
+#include <JuceHeader.h>
+
+class Sequencer : public juce::AudioSource
+{
+public:
+  Sequencer(const juce::Array<juce::uint8>& sequence);
+
+  /*
+  *  Set the tempo of this Sequencer.
+  *
+  *  @param notesPerMinute how quickly to change sequencer steps,
+                           e.g. 60.0 times per minute
+  */
+  void setTempo(double notesPerMinute);
+
+  virtual void prepareToPlay(
+      int samplesPerBlockExpected, double sampleRate) override;
+
+  virtual void releaseResources() override;
+
+  virtual void getNextAudioBlock(
+      const juce::AudioSourceChannelInfo &bufferToFill) override;
+
+private:
+  double notesPerMinute_;
+  juce::Array<juce::uint8> sequence_;
+};
